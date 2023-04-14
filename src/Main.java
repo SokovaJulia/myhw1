3fdb7432ba7e649df2eccc82bf760496905ee787
julia sokova <jlia_1990@mail.ru>
2023-04-14 20:03:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13_676;
+        int miles = price / 20;
+
+        System.out.println("Бонус:" + miles);
+    }
+}
